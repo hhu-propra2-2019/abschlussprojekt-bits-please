3d74ef46d7394d60975c338e9b4dbe335ef7f3db
SCroize <49495761+SCroize@users.noreply.github.com>
2020-03-03 14:00:59 +0100

    Add Spring Project

---
+++ zulassung2/src/main/java/mops/zulassung2/Zulassung2Application.java
@@ -0,0 +1,13 @@
+package mops.zulassung2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zulassung2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zulassung2Application.class, args);
+	}
+
+}
---
+++ zulassung2/src/test/java/mops/zulassung2/Zulassung2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mops.zulassung2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zulassung2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
