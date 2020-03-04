0bba88d8181ea45f754e8691f6788c8465530881
cobizobi <jan.gruteser@hhu.de>
2020-03-04 14:01:04 +0100

    Add Keycloak/Security Configuration

---
+++ zulassung2/src/main/java/mops/zulassung2/KeycloakConfig.java
@@ -0,0 +1,13 @@
+package mops.zulassung2;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+	@Bean
+	public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
+}
