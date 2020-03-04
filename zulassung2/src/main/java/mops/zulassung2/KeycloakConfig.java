c9af9f1255808f2f1b0a16e3cafca8b76dd42969
cobizobi <jan.gruteser@hhu.de>
2020-03-04 16:52:06 +0100

    Fix star import and method name

--- zulassung2/src/main/java/mops/zulassung2/KeycloakConfig.java
+++ zulassung2/src/main/java/mops/zulassung2/KeycloakConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KeycloakConfig {
   @Bean
-  public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+  public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
     return new KeycloakSpringBootConfigResolver();
   }
 }
