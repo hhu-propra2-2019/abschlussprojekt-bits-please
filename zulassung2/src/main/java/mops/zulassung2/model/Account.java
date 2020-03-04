ac63e5f72f90a622b8c08d9fafbbd4bba1a6230f
masie127 <masie127@uni-duesseldorf.de>
2020-03-04 16:33:38 +0100

    change working directory

---
+++ zulassung2/src/main/java/mops/zulassung2/model/Account.java
@@ -0,0 +1,15 @@
+package mops.zulassung2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class Account {
+    private final String name;
+    private final String email;
+    private final String image;
+    private final Set<String> roles;
+}
