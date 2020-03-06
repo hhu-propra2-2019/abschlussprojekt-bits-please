bd7aedc19c213afd592f85cc7fa1f9eaaf287547
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-06 14:34:31 +0100

    Add Test For Class KeyConverter

--- zulassung2/src/main/java/mops/zulassung2/KeyConverter.java
+++ zulassung2/src/main/java/mops/zulassung2/KeyConverter.java
@@ -1,27 +1,19 @@
 package mops.zulassung2;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class KeyConverter {
 
-  @Value("${dev_private_key}")
-  private String devPrivateKeyAsString;
-
-  @Value("${dev_public_key}")
-  private String devPublicKeyAsString;
-
-  public SecretKey getPrivateKey() {
-    byte[] decodedPrivateKey = Base64.getDecoder().decode(devPrivateKeyAsString);
+  public SecretKey getPrivateKey(String privateKeyAsString) {
+    byte[] decodedPrivateKey = Base64.getDecoder().decode(privateKeyAsString);
     SecretKey secretPrivateKey = new SecretKeySpec(decodedPrivateKey, 0, decodedPrivateKey.length, "RSA");
     return secretPrivateKey;
   }
 
-  public SecretKey getPublicKey() {
-    byte[] decodedPublicKey = Base64.getDecoder().decode(devPublicKeyAsString);
+  public SecretKey getPublicKey(String publicKeyAsString) {
+    byte[] decodedPublicKey = Base64.getDecoder().decode(publicKeyAsString);
     SecretKey secretPublicKey = new SecretKeySpec(decodedPublicKey, 0, decodedPublicKey.length, "RSA");
     return secretPublicKey;
   }
--- zulassung2/src/test/java/mops/zulassung2/KeyConverterTest.java
+++ zulassung2/src/test/java/mops/zulassung2/KeyConverterTest.java
@@ -1,4 +1,32 @@
 package mops.zulassung2;
 
-public class KeyConverterTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KeyConverterTest {
+
+  @Value("${dev_private_key}")
+  String privateKeyAsString;
+
+  @Test
+  void testThatPublicKeyIsConvertedCorrectlyFromString() {
+    // Arrange
+    KeyConverter keyConverter = new KeyConverter();
+
+    // Act
+    SecretKey privateKey = keyConverter.getPrivateKey(privateKeyAsString);
+
+    // Assert
+    String test = privateKey.
+        getFormat();
+
+    assertThat(privateKeyAsString).isEqualTo(privateKey.toString());
+
+
+  }
+
 }
