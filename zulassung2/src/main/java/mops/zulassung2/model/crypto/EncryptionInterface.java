efc263096413a84a68cf1aa5d105dc444da62608
cobizobi <jan.gruteser@hhu.de>
2020-03-06 14:39:14 +0100

    Fix Checkstyle

--- zulassung2/src/main/java/mops/zulassung2/model/crypto/EncryptionInterface.java
+++ zulassung2/src/main/java/mops/zulassung2/model/crypto/EncryptionInterface.java
@@ -3,5 +3,6 @@ package mops.zulassung2.model.crypto;
 public interface EncryptionInterface {
 
   public byte[] encrypt(String toEncrypt);
+
   public boolean decrypt(String toDecrypt, String toVerify);
 }
--- zulassung2/src/main/java/mops/zulassung2/model/crypto/SignaturService.java
+++ zulassung2/src/main/java/mops/zulassung2/model/crypto/SignaturService.java
@@ -1,7 +1,15 @@
 package mops.zulassung2.model.crypto;
 
+import java.nio.charset.StandardCharsets;
+
 public class SignaturService {
 
+  /**
+   *  Creates a signature for a given String.
+   *
+   * @param toSign data to be signed
+   * @return Receipt which contains data and signature
+   */
   public Receipt sign(String toSign) {
 
     HasherInterface hasherInterface = new Hasher();
@@ -10,15 +18,24 @@ public class SignaturService {
     EncryptionInterface encryptionInterface = new Encryption();
     byte[] signed = encryptionInterface.encrypt(hashed);
 
-    return new Receipt(toSign, new String(signed));
+    return new Receipt(toSign, new String(signed, StandardCharsets.UTF_8));
   }
 
-  public boolean verify(Receipt toVerify){
+  /**
+   *  Verifies the signature and validity for a given Receipt.
+   *
+   * @param toVerify Receipt to be verified
+   * @return true, if valid; false, if invalid
+   */
+  public boolean verify(Receipt toVerify) {
 
     HasherInterface hasherInterface = new Hasher();
     EncryptionInterface encryptionInterface = new Encryption();
 
-    return encryptionInterface.decrypt(hasherInterface.getHash(toVerify.getData()), toVerify.getSignature());
+    String data = toVerify.getData();
+    String signature = toVerify.getSignature();
+
+    return encryptionInterface.decrypt(hasherInterface.getHash(data), signature);
   }
 
 }
