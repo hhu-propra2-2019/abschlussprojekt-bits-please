59ba8fe16a6cc2da53b24afc2bcf278974772644
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-06 15:09:32 +0100

    Replace SecretKey with PublicKey and PrivateKey as method return value

--- zulassung2/src/main/java/mops/zulassung2/KeyConverter.java
+++ zulassung2/src/main/java/mops/zulassung2/KeyConverter.java
@@ -1,21 +1,28 @@
 package mops.zulassung2;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class KeyConverter {
 
-  public SecretKey getPrivateKey(String privateKeyAsString) {
+  public PrivateKey getPrivateKey(String privateKeyAsString) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
     byte[] decodedPrivateKey = Base64.getDecoder().decode(privateKeyAsString);
-    SecretKey secretPrivateKey = new SecretKeySpec(decodedPrivateKey, 0, decodedPrivateKey.length, "RSA");
-    return secretPrivateKey;
+    PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedPrivateKey);
+    return keyFactory.generatePrivate(keySpec);
   }
 
-  public SecretKey getPublicKey(String publicKeyAsString) {
+  public PublicKey getPublicKey(String publicKeyAsString) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
     byte[] decodedPublicKey = Base64.getDecoder().decode(publicKeyAsString);
-    SecretKey secretPublicKey = new SecretKeySpec(decodedPublicKey, 0, decodedPublicKey.length, "RSA");
-    return secretPublicKey;
+    X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedPublicKey);
+    return keyFactory.generatePublic(keySpec);
   }
 
 }
