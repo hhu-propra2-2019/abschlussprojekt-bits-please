2603e046c6b89a1af279339129a869b741c465e2
cobizobi <49492914+cobizobi@users.noreply.github.com>
2020-03-06 16:15:10 +0100

    Changed route to zulassung2 (#37)

--- zulassung2/src/main/java/mops/zulassung2/controller/OrganisatorController.java
+++ zulassung2/src/main/java/mops/zulassung2/controller/OrganisatorController.java
@@ -7,7 +7,9 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/zulassung2")
 @Controller
 public class OrganisatorController {
 
