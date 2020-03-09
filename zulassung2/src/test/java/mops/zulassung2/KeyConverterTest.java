01886374d408e0b713655c06e4a60efa444b5a44
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-09 16:21:12 +0100

    Clear Checkstyle and Spotbugs Errors

--- zulassung2/src/test/java/mops/zulassung2/KeyConverterTest.java
+++ zulassung2/src/test/java/mops/zulassung2/KeyConverterTest.java
@@ -1,21 +1,27 @@
 package mops.zulassung2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 class KeyConverterTest {
 
   @Test
-  void testEncryptionAndDecryption() throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+  void testEncryptionAndDecryption()
+      throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException,
+      InvalidKeyException, BadPaddingException, IllegalBlockSizeException,
+      UnsupportedEncodingException {
+
     //Arrange
     KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
     keyGen.initialize(1024);
@@ -30,11 +36,11 @@ class KeyConverterTest {
     PrivateKey privateKey = keyConverter.getPrivateKey(testPrivateKeyAsString);
     Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
     cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-    String encodedMessage = Base64.encodeBase64String(cipher.doFinal(message.getBytes()));
+    String encodedMessage = Base64.encodeBase64String(cipher.doFinal(message.getBytes("UTF-8")));
 
     PublicKey publicKey = keyConverter.getPublicKey(testPublicKeyAsString);
     cipher.init(Cipher.DECRYPT_MODE, publicKey);
-    String resultMessage = new String(cipher.doFinal(Base64.decodeBase64(encodedMessage)));
+    String resultMessage = new String(cipher.doFinal(Base64.decodeBase64(encodedMessage)), "UTF-8");
 
     // Assert
     assertThat(message).isEqualTo(resultMessage);
