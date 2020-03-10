4de71cde3d5ace61a162d9b7946c2b9c5e433d5d
Tim Geuer <tigeu100@hhu.de>
2020-03-10 13:25:27 +0100

    Add test of CSV parsing.

--- zulassung2/src/main/java/mops/zulassung2/model/Studentin.java
+++ zulassung2/src/main/java/mops/zulassung2/model/Studentin.java
@@ -3,6 +3,8 @@ package mops.zulassung2.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @Getter
 public class Studentin {
@@ -11,4 +13,25 @@ public class Studentin {
   private String email;
   private String name;
   private String vorname;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Studentin studentin = (Studentin) o;
+    return Objects.equals(matrikelnummer, studentin.matrikelnummer)
+            && Objects.equals(email, studentin.email)
+            && Objects.equals(name, studentin.name)
+            && Objects.equals(vorname, studentin.vorname);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(matrikelnummer, email, name, vorname);
+  }
 }
--- zulassung2/src/test/java/mops/zulassung2/ParseFileTests.java
+++ zulassung2/src/test/java/mops/zulassung2/ParseFileTests.java
@@ -1,4 +1,35 @@
 package mops.zulassung2;
 
-public class ParseFileTests {
+import mops.zulassung2.model.CSVParser;
+import mops.zulassung2.model.Studentin;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParseFileTests {
+
+  @Test
+  public void testParsingStudentsFromCSV() {
+    // Arrange
+    List<Studentin> students = new ArrayList<>();
+    Studentin tim = new Studentin("2727912", "tigeu100@hhu.de", "geuer", "tim");
+    Studentin markus = new Studentin("2757144", "masie@hhu.de", "siewert", "markus");
+    students.add(tim);
+    students.add(markus);
+
+    String data = "2727912,tigeu100@hhu.de,geuer,tim\n2757144,masie@hhu.de,siewert,markus\n";
+    CSVParser parser = new CSVParser(",");
+    MockMultipartFile csvFile = new MockMultipartFile("testFile,csv", "test_students.csv", "text/csv", data.getBytes(StandardCharsets.UTF_8));
+
+    // Act
+    List<Studentin> parsedStudents = parser.processCSV(csvFile);
+
+    // Assert
+    assertThat(parsedStudents).isEqualTo(students);
+  }
 }
