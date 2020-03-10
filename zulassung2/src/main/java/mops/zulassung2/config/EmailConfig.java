465be95f6288a58374c0ae7836bc29adb29b3491
cobizobi <jan.gruteser@hhu.de>
2020-03-10 12:57:35 +0100

    Fix EmailConfig

--- zulassung2/src/main/java/mops/zulassung2/config/EmailConfig.java
+++ zulassung2/src/main/java/mops/zulassung2/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package mops.zulassung2.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,6 +11,15 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+  @Value("${spring.mail.host}")
+  String host;
+  @Value("${spring.mail.port}")
+  int port;
+  @Value("${spring.mail.username}")
+  String username;
+  @Value("${spring.mail.password}")
+  String password;
+
   /**
    * Configures settings for sending emails via SMTP.
    *
@@ -18,11 +28,11 @@ public class EmailConfig {
   @Bean
   public JavaMailSender getJavaMailSender() {
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("securesmtp.t-online.de");
-    mailSender.setPort(465);
+    mailSender.setHost(host);
+    mailSender.setPort(port);
 
-    mailSender.setUsername("mops.zulassung2@magenta.de");
-    mailSender.setPassword("zulassung2");
+    mailSender.setUsername(username);
+    mailSender.setPassword(password);
 
     Properties props = mailSender.getJavaMailProperties();
     props.put("mail.transport.protocol", "smtp");
