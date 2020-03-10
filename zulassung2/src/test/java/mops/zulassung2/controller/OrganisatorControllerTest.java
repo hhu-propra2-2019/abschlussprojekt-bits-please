f0357e6f6a0e61d08ec26d36ec5f318216ad2349
cobizobi <jan.gruteser@hhu.de>
2020-03-10 15:36:34 +0100

    Add OrganisatorControllerTest

---
+++ zulassung2/src/test/java/mops/zulassung2/controller/OrganisatorControllerTest.java
@@ -0,0 +1,42 @@
+package mops.zulassung2.controller;
+
+import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(OrganisatorController.class)
+@ComponentScan(
+    basePackageClasses = {KeycloakSecurityComponents.class, KeycloakSpringBootConfigResolver.class})
+class OrganisatorControllerTest {
+
+  @Autowired
+  MockMvc mvc;
+
+  @Test
+  @WithMockKeycloackAuth("studentin")
+  void orgaAsStudent() throws Exception {
+    mvc.perform(get("/zulassung2/orga")).andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockKeycloackAuth("orga")
+  void orgaAsOrga() throws Exception {
+    mvc.perform(get("/zulassung2/orga")).andExpect(status().isOk());
+  }
+
+  @Test
+  void orgaAsUnauthorized() throws Exception {
+    mvc.perform(get("/zulassung2/orga")).andExpect(status().is(302));
+  }
+}
