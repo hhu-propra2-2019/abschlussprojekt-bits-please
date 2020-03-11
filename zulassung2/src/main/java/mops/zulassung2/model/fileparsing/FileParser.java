c925cff0aa7cfc3639cc9d4a6efeb1c28d5d3dd4
Tim Geuer <tigeu100@hhu.de>
2020-03-11 13:43:48 +0100

    Rearrange structure of FileParser.

--- zulassung2/src/main/java/mops/zulassung2/model/fileparsing/FileParser.java
+++ zulassung2/src/main/java/mops/zulassung2/model/fileparsing/FileParser.java
@@ -1,5 +1,6 @@
-package mops.zulassung2.model;
+package mops.zulassung2.model.fileparsing;
 
+import mops.zulassung2.model.Student;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -10,12 +11,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class FileParser {
+
+  private Validator validator;
+  private CSVLineParser csvLineParser;
+
+  public FileParser(Validator validator) {
+    this.validator = validator;
+    this.csvLineParser = new CustomCSVLineParser();
+  }
+
+  public FileParser(Validator validator, CSVLineParser csvLineParser) {
+    this.validator = validator;
+    this.csvLineParser = csvLineParser;
+  }
 
   /**
    * Diese Methode wird vom OrganisatorController aufgerufen, nachdem eine File hochgeladen wurde.
@@ -32,41 +44,21 @@ public class FileParser {
     CSVParser csvParser = null;
 
     try {
-      Path test = studentFile.toPath();
       reader = Files.newBufferedReader(studentFile.toPath());
       csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    if (csvParser == null) {
-      deleteFile(studentFile);
-      return null;
-    }
-    Map<String, Integer> headerMap = csvParser.getHeaderMap();
-    if (headerMap.size() != 4) {
-      deleteFile(studentFile);
-      return null;
-    }
+    boolean csvIsValid = validator.validateCSV(csvParser);
 
-    for (String key : headerMap.keySet()) {
-      Integer index = headerMap.get(key);
-      if ((index == 0 && !key.equals("matriculationnumber"))
-              || (index == 1 && !key.equals("email"))
-              || (index == 2 && !key.equals("name"))
-              || (index == 3 && !key.equals("forname"))) {
-        deleteFile(studentFile);
-        return null;
-      }
+    if (!csvIsValid) {
+      deleteFile(studentFile);
+      return null;
     }
 
     for (CSVRecord csvRecord : csvParser) {
-      String matrNr = csvRecord.get(0);
-      String email = csvRecord.get(1);
-      String name = csvRecord.get(2);
-      String forename = csvRecord.get(3);
-
-      Student currentStudent = new Student(matrNr, email, name, forename);
+      Student currentStudent = csvLineParser.parseLine(csvRecord);
       studentList.add(currentStudent);
     }
 
