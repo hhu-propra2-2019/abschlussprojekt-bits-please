f2ad135b966f71a027fafb0b388d1b93012ce951
tigeu <49535253+tigeu@users.noreply.github.com>
2020-03-11 17:00:06 +0100

    Adapt structure (#65)
    * Adapt structure and extract logic to service.
    
    * Add missing javadoc comment.
    
    * Adapt Controller structure.
    
    * Fix controller tests.
    
    * Fix checkstyle error.
    
    * Fix checkstyle error 2.
    
    * Fix Checkstyleerror 3.

--- zulassung2/src/test/java/mops/zulassung2/controller/OrganisatorControllerTest.java
+++ zulassung2/src/test/java/mops/zulassung2/controller/OrganisatorControllerTest.java
@@ -1,12 +1,14 @@
 package mops.zulassung2.controller;
 
 import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import mops.zulassung2.services.OrganisatorService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,12 +18,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(OrganisatorController.class)
-@ComponentScan(
-    basePackageClasses = {KeycloakSecurityComponents.class, KeycloakSpringBootConfigResolver.class})
+@ComponentScan(basePackageClasses = {KeycloakSecurityComponents.class,
+        KeycloakSpringBootConfigResolver.class})
 class OrganisatorControllerTest {
 
   @Autowired
   MockMvc mvc;
+
+  @MockBean
+  OrganisatorService organisatorService;
 
   @Test
   @WithMockKeycloackAuth("studentin")
