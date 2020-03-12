aeaa9ca80ef544df95d211c9e809d8fbd6b70eea
cobizobi <jan.gruteser@hhu.de>
2020-03-12 10:56:24 +0100

    Add Javadoc comments

--- zulassung2/src/main/java/mops/zulassung2/model/crypto/Encryption.java
+++ zulassung2/src/main/java/mops/zulassung2/model/crypto/Encryption.java
@@ -11,6 +11,13 @@ public class Encryption implements EncryptionInterface {
 
   private static final Logger logger = LoggerFactory.getLogger(Zulassung2Application.class);
 
+  /**
+   * Signs a given String with a given PrivateKey.
+   *
+   * @param toEncrypt  String to sign
+   * @param privateKey Key to encrypt signature
+   * @return signature of toEncrypt
+   */
   public byte[] encrypt(String toEncrypt, PrivateKey privateKey) {
     try {
       SecureRandom secureRandom = new SecureRandom();
@@ -30,6 +37,15 @@ public class Encryption implements EncryptionInterface {
 
   }
 
+  /**
+   * Checks the validity of the given string together with its
+   * signature by using the given PublicKey for decryption.
+   *
+   * @param toDecrypt String to verify together with its signature
+   * @param toVerify  signature of the String to be verified
+   * @param publicKey Key to decrypt signature
+   * @return true if the pair of text and signature is valid, otherwise false
+   */
   public boolean decrypt(String toDecrypt, String toVerify, PublicKey publicKey) {
     try {
       Signature signature = Signature.getInstance("SHA256WithRSA");
