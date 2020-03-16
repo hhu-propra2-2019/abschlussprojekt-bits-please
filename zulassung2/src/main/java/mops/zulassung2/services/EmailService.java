b445b6c4c39d3194623a84c363eff079ee73f5cd
Snur <snami100@uni-duesseldorf.de>
2020-03-16 14:59:58 +0100

    refactor E-Mail Service
    Co-Authored-By: douglas-bl <douglas-bl@users.noreply.github.com>
    Co-Authored-By: tigeu <tigeu@users.noreply.github.com>
    Co-Authored-By: masie127 <masie127@users.noreply.github.com>

--- zulassung2/src/main/java/mops/zulassung2/services/EmailService.java
+++ zulassung2/src/main/java/mops/zulassung2/services/EmailService.java
@@ -20,82 +20,86 @@ import java.nio.charset.StandardCharsets;
 @Service
 public class EmailService {
 
-  private static final Logger logger = LoggerFactory.getLogger(Zulassung2Application.class);
-  private final JavaMailSender emailSender;
-  private final SignatureService signatureService;
-  @Value("${email_body_text}")
-  private String emailBodyText;
+    private static final Logger logger = LoggerFactory.getLogger(Zulassung2Application.class);
+    private final JavaMailSender emailSender;
+    private final SignatureService signatureService;
+    @Value("${email_body_text}")
+    private String emailBodyText;
 
-  public EmailService(JavaMailSender emailSender, SignatureService signatureService) {
-    this.emailSender = emailSender;
-    this.signatureService = signatureService;
-  }
-
-  /**
-   * Sends an email with attachment (File).
-   *
-   * @param to       receiver of the mail
-   * @param subject  subject of the mail
-   * @param attach   file to be attached
-   * @param filename name of the attached file
-   */
-  public void sendMessage(String to, String subject, String text, File attach, String filename) {
-
-    try {
-      MimeMessage message = emailSender.createMimeMessage();
-
-      MimeMessageHelper helper = new MimeMessageHelper(message, true);
-      helper.setTo(to);
-      helper.setSubject(subject);
-      helper.setText(text);
-      helper.addAttachment(filename, attach);
-
-      emailSender.send(message);
-
-    } catch (MessagingException e) {
-      logger.error(e.getMessage());
-    }
-  }
-
-  /**
-   * Diese Methode wird vom OrganisatorController (Methode: sendMail) aufgerufen.
-   * *
-   * Diese Methode erstellt benutzerdefinierte Files und ruft sendMessage auf.
-   */
-  public void createFileAndMail(ReceiptData receiptData) {
-    File file = new File(System.getProperty("user.dir")
-        + "token_" + receiptData.getModule()
-        + "_" + receiptData.getName() + ".txt");
-    FileWriter writer;
-    String data = receiptData.create();
-    Receipt receipt = signatureService.sign(data);
-    try {
-      writer = new FileWriter(file, StandardCharsets.UTF_8);
-      writer.write(data + "\n");
-      writer.write(receipt.getSignature());
-      writer.close();
-    } catch (IOException e) {
-      e.printStackTrace();
+    public EmailService(JavaMailSender emailSender, SignatureService signatureService) {
+        this.emailSender = emailSender;
+        this.signatureService = signatureService;
     }
 
-    Student student = new Student(receiptData.getMatriculationNumber(),
-        receiptData.getEmail(),
-        receiptData.getName(),
-        receiptData.getForeName());
-    String customizedEmailBodyText =
-        createCustomizedEmailBodyText(student, receiptData.getModule());
-    String mail = receiptData.getEmail();
-    sendMessage(mail, "Ihr Zulassungsnachweis zum Fach " + receiptData.getModule(),
-        customizedEmailBodyText, file, file.getName());
+    /**
+     * Sends an email with attachment (File).
+     *
+     * @param to       receiver of the mail
+     * @param subject  subject of the mail
+     * @param attach   file to be attached
+     * @param filename name of the attached file
+     */
+    public void sendMessage(String to, String subject, String text, File attach, String filename) {
 
-    file.deleteOnExit();
-  }
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
 
-  private String createCustomizedEmailBodyText(Student student, String currentSubject) {
-    String customizedEmailBodyText = emailBodyText;
-    customizedEmailBodyText = customizedEmailBodyText.replace(":name", student.getName());
-    customizedEmailBodyText = customizedEmailBodyText.replace(":modul", currentSubject);
-    customizedEmailBodyText = customizedEmailBodyText.replace(":break", "\n");
-    return customizedEmailBodyText;
-  }
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            helper.addAttachment(filename, attach);
+
+            emailSender.send(message);
+
+        } catch (MessagingException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Diese Methode wird vom OrganisatorController (Methode: sendMail) aufgerufen.
+     * *
+     * Diese Methode erstellt benutzerdefinierte Files und ruft sendMessage auf.
+     */
+
+    public File createFile(ReceiptData receiptData, String data, Receipt receipt) {
+        File file = new File(System.getProperty("user.dir")
+                + "token_" + receiptData.getModule()
+                + "_" + receiptData.getName() + ".txt");
+        FileWriter writer;
+
+        try {
+            writer = new FileWriter(file, StandardCharsets.UTF_8);
+            writer.write(data + "\n");
+            writer.write(receipt.getSignature());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+
+    public void sendMail(Student student, String currentSubject) {
+        ReceiptData receiptData = new CustomReceiptData(student, currentSubject);
+        String data = receiptData.create();
+        Receipt receipt = signatureService.sign(data);
+        File file = createFile(receiptData, data, receipt);
+
+        String emailText = createCustomizedEmailBodyText(student, currentSubject);
+        String mail = receiptData.getEmail();
+        String subject = "Ihr Zulassungsnachweis zum Fach: ";
+        sendMessage(mail, subject + currentSubject, emailText, file, file.getName());
+
+        file.deleteOnExit();
+    }
+
+    private String createCustomizedEmailBodyText(Student student, String currentSubject) {
+        String customizedEmailBodyText = emailBodyText;
+        customizedEmailBodyText = customizedEmailBodyText.replace(":name", student.getName());
+        customizedEmailBodyText = customizedEmailBodyText.replace(":modul", currentSubject);
+        customizedEmailBodyText = customizedEmailBodyText.replace(":break", "\n");
+        return customizedEmailBodyText;
+    }
 }
