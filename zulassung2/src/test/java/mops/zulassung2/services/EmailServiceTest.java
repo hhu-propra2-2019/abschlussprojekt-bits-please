0345d17ff97e641f60c80222458140a7117541b6
Douglas-Bl <Douglas-Blank@gmx.de>
2020-03-17 15:15:16 +0100

    add test for createFile

--- zulassung2/src/test/java/mops/zulassung2/services/EmailServiceTest.java
+++ zulassung2/src/test/java/mops/zulassung2/services/EmailServiceTest.java
@@ -1,5 +1,6 @@
 package mops.zulassung2.services;
 
+import mops.zulassung2.model.crypto.Receipt;
 import mops.zulassung2.model.dataobjects.Student;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,17 @@ import org.mockito.Mockito;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class EmailServiceTest {
 
+  private Student douglas = new Student("2729350", "dobla101@hhu.de", "blank", "douglas");
   static EmailService emailServices = mock(EmailService.class);
   static EmailService emailService;
   static SignatureService signatureService = mock(SignatureService.class);
@@ -31,5 +38,32 @@ class EmailServiceTest {
     emailServices.sendMail(student, subject, file);
 
     Mockito.verify(emailServices).sendMail(student, subject, file);
+  }
+
+  @Test
+  void createFile() throws IOException {
+    // Arrange
+    ReceiptData receiptData = mock(ReceiptData.class);
+    String data = "matriculationnumber:" + douglas.getMatriculationNumber()
+            + " email:" + douglas.getEmail()
+            + " name:" + douglas.getName()
+            + " forename:" + douglas.getForeName()
+            + " module:" + "informatik"
+            + " semester:" + "null"
+            + "\n"
+            + "signatur";
+    Receipt receipt = mock(Receipt.class);
+    when(receiptData.getModule()).thenReturn("informatik");
+    when(receiptData.getName()).thenReturn("blank");
+    when(receipt.getSignature()).thenReturn("signatur");
+    Receipt receipt1 = new Receipt(data,"signatur");
+    when(signatureService.sign(anyString())).thenReturn(receipt1);
+
+    // Act
+    File file = emailService.createFile(douglas, "informatik");
+    String content = Files.readString(file.toPath());
+
+    // Assert
+    assertEquals(data, content);
   }
 }
