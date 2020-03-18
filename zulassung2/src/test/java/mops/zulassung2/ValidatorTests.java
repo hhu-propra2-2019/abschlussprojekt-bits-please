0096de8f7cfda78f5e76ed46a2277dee45234aca
loalf100 <loalf100@uni-duesseldorf.de>
2020-03-18 15:35:19 +0100

    validateTextTest and validateCSVTest

--- zulassung2/src/test/java/mops/zulassung2/ValidatorTests.java
+++ zulassung2/src/test/java/mops/zulassung2/ValidatorTests.java
@@ -1,55 +1,66 @@
 package mops.zulassung2;
-import static org.junit.jupiter.api.Assertions.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+import mops.zulassung2.model.dataobjects.Student;
 import mops.zulassung2.model.fileparsing.CustomValidator;
+import mops.zulassung2.services.CustomReceiptData;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidatorTests {
   private CustomValidator customValidator;
+  private Student student;
+  private CustomReceiptData customReceiptData;
+
+  @Test
+  public void validateTextTest() throws IOException {
+    customValidator = new CustomValidator();
+    student = new Student("9999999", "test@uni-duesseldorf.de", "meier", "max");
+    String currentSubject = "informatik";
+    String semster = "zwei";
+    customReceiptData = new CustomReceiptData(student, currentSubject, semster);
+    File file = new File("test.txt");
+    FileWriter writer = null;
+    try {
+      writer = new FileWriter(file, UTF_8);
+      writer.write(customReceiptData.create());
+      writer.write("\n.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      //close resources
+      try {
+        writer.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    String receiptContent = Files.readString(file.toPath(), StandardCharsets.UTF_8);
+    List<String> lines = receiptContent.lines().collect(Collectors.toList());
+    assertThat(customValidator.validateTXT(lines)).isTrue();
+  }
 
   @Test
   public void validateCSVTest() throws IOException {
+    customValidator =new CustomValidator();
+    final CSVParser parser = CSVParser.parse("9999999\ntest@uni-duesseldorf.de\nmeier\nmax",
+        CSVFormat.DEFAULT.withHeader("matriculationnumber", "email", "name","forename"));
+   assertThat(customValidator.validateCSV(parser)).isTrue();
 
-      final CSVParser parser = CSVParser.parse("9999999\ntest@uni-duesseldorf.de\nmeier\nmax",
-          CSVFormat.DEFAULT.withHeader("matriculationnumber", "email", "name","forename"));
 
-    final Map<String, Integer> headerMap = parser.getHeaderMap();
-    final Iterator<String> columnNames = headerMap.keySet().iterator();
-    assertEquals("matriculationnumber", columnNames.next());
-    assertEquals("email", columnNames.next());
-    assertEquals("name", columnNames.next());
-    assertEquals("forename", columnNames.next());
-    final Iterator<CSVRecord> records = parser.iterator();
-
-    // Parse to make sure getHeaderMap did not have a side-effect.
-    for (int i = 0; i < 4; i++) {
-      assertTrue(records.hasNext());
-      final CSVRecord record = records.next();
-      assertEquals(record.get(0), record.get("matriculationnumber"));
-      assertEquals(record.get(1), record.get("email"));
-      assertEquals(record.get(2), record.get("name"));
-      assertEquals(record.get(3), record.get("forename"));
-    }
-
-    assertFalse(records.hasNext());
-  }
 
   }
 
 
-
-
-
-
-
-
-
-
-
+}
