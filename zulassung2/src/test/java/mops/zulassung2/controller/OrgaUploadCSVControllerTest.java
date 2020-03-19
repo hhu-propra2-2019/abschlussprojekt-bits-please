3319df23e52c46470923ce920e4dc7463b8353f6
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-19 13:38:54 +0100

    Add test

--- zulassung2/src/test/java/mops/zulassung2/controller/OrgaUploadCSVControllerTest.java
+++ zulassung2/src/test/java/mops/zulassung2/controller/OrgaUploadCSVControllerTest.java
@@ -4,6 +4,7 @@ import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackA
 import mops.zulassung2.services.EmailService;
 import mops.zulassung2.services.OrganisatorService;
 import mops.zulassung2.services.SignatureService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
@@ -12,10 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -24,16 +30,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     KeycloakSpringBootConfigResolver.class})
 class OrgaUploadCSVControllerTest {
 
+  static MockMultipartFile mockCsvFile;
   @Autowired
   MockMvc mvc;
   @MockBean
   EmailService emailService;
-
-
   @MockBean
   OrganisatorService organisatorService;
   @MockBean
   SignatureService signatureService;
+
+  @BeforeAll
+  static void createFile() {
+/*
+    String header = "matriculationnumber,email,name,forename\n";
+    String student1Data = "2727912,tigeu100@hhu.de,geuer,tim\n";
+    String student2Data = "2757144,masie@hhu.de,siewert,markus\n";
+    String data = header + student1Data + student2Data;
+    byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
+    String name = "testFile.csv";
+    String originalFileName = "test_students.csv";
+    MockMultipartFile mockCsvFile = new MockMultipartFile(name, originalFileName, "text/csv", bytes);
+*/
+
+    mockCsvFile = new MockMultipartFile(
+        "MockCsvFile",
+        "Receipt.csv",
+        "text/csv",
+        "Test Data".getBytes(StandardCharsets.UTF_8));
+  }
 
   @Test
   @WithMockKeycloackAuth("studentin")
@@ -51,4 +76,26 @@ class OrgaUploadCSVControllerTest {
   void orgaCSVAsUnauthorizedIsRedirected() throws Exception {
     mvc.perform(get("/zulassung2/orga/upload-csv")).andExpect(status().is(302));
   }
+
+  @Test
+  @WithMockKeycloackAuth("orga")
+  void testSubmitCsvFile() throws Exception {
+    mvc.perform(multipart("/zulassung2/orga/upload-csv")
+        .file(mockCsvFile)
+        .param("subject", "Propra")
+        .param("semester", "WS1920")
+        .with(csrf()))
+        .andExpect(redirectedUrl("/zulassung2/orga/upload-csv"));
+  }
+
+  @Test
+  @WithMockKeycloackAuth("orga")
+  void testSendMails() throws Exception {
+    // Arrange
+
+    // Act & Assert
+    mvc.perform(post("/zulassung2/orga/sendmail")).andExpect(status().is(302));
+  }
+
+  // testen dass falsche Rolle Forbidden ist
 }
