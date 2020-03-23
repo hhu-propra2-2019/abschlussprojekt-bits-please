c27a654e71e6abadf638ec8b1546841db8f08cc9
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-23 13:42:34 +0100

    Add tests for sending and uploading

--- zulassung2/src/test/java/mops/zulassung2/controller/OrgaUploadCSVControllerTest.java
+++ zulassung2/src/test/java/mops/zulassung2/controller/OrgaUploadCSVControllerTest.java
@@ -5,25 +5,26 @@ import mops.zulassung2.model.dataobjects.Student;
 import mops.zulassung2.services.EmailService;
 import mops.zulassung2.services.OrganisatorService;
 import mops.zulassung2.services.SignatureService;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -35,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     KeycloakSpringBootConfigResolver.class})
 class OrgaUploadCSVControllerTest {
 
-  static MockMultipartFile mockCsvFile;
+  MockMultipartFile mockCsvFile;
   @Autowired
   MockMvc mvc;
   @MockBean
@@ -45,20 +46,34 @@ class OrgaUploadCSVControllerTest {
   @MockBean
   SignatureService signatureService;
 
-  @BeforeAll
-  static void createFile() {
-    String header = "matriculationnumber,email,name,forename\n";
-    String student1Data = "2727912,tigeu100@hhu.de,geuer,tim\n";
-    String student2Data = "2757144,masie@hhu.de,siewert,markus\n";
-    String data = header + student1Data + student2Data;
-    byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-    mockCsvFile = new MockMultipartFile("file", "originalFileName", "text/csv", bytes);
+  @BeforeEach
+  void createFile() {
+    mockCsvFile = new MockMultipartFile(
+        "file",
+        "file.csv",
+        "text/csv",
+        "Test Data".getBytes(StandardCharsets.UTF_8));
   }
 
   @Test
   @WithMockKeycloackAuth("studentin")
   void orgaCSVAsStudentIsRefused() throws Exception {
+    // Arrange
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    List<Student> students = new ArrayList<>();
+    students.add(mock(Student.class));
+    when(organisatorService.processCSVUpload(mockCsvFile)).thenReturn(students);
+
+    // Act and Assert
     mvc.perform(get("/zulassung2/orga/upload-csv"))
+        .andExpect(status().isForbidden());
+    mvc.perform(post("/zulassung2/orga/sendmail")
+        .with(csrf()))
+        .andExpect(status().isForbidden());
+    mvc.perform(post("/zulassung2/orga/sendmail/individual")
+        .param("count", "0")
+        .session(mockHttpSession)
+        .with(csrf()))
         .andExpect(status().isForbidden());
   }
 
@@ -99,34 +114,25 @@ class OrgaUploadCSVControllerTest {
   @WithMockKeycloackAuth("orga")
   void testSendMailToIndividualStudent() throws Exception {
     // Arrange
-    Student stud = new Student("2727912", "tigeu100@hhu.de", "geuer", "tim");
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    List<Student> students = new ArrayList<>();
+    // Student student = new Student("2727912", "tigeu100@hhu.de", "geuer", "tim");
+    students.add(mock(Student.class));
+    when(organisatorService.processCSVUpload(mockCsvFile)).thenReturn(students);
+
     mvc.perform(multipart("/zulassung2/orga/upload-csv")
         .file(mockCsvFile)
         .param("subject", "Propra")
         .param("semester", "WS1920")
+        .session(mockHttpSession)
         .with(csrf()));
-
-    ArgumentCaptor<Student> studentCaptor = ArgumentCaptor.forClass(Student.class);
-    ArgumentCaptor<File> fileCaptor = ArgumentCaptor.forClass(File.class);
-    verify(emailService, times(1)).sendMail(
-        studentCaptor.capture(), "Ihr Zulassungsnachweis zum Fach: ", fileCaptor.capture());
-    // assertThat(studentCaptor.getValue().getForeName()).isEqualTo("sth");
-
-    /*    OrgaUploadCSVController orgaUploadCSVController
-        = new OrgaUploadCSVController(organisatorService, emailService);
-
-    orgaUploadCSVController.students.add(new Student(
-        "2727912",
-        "tigeu100@hhu.de",
-        "geuer",
-        "tim"));*/
-
 
     // Act and Assert
     mvc.perform(post("/zulassung2/orga/sendmail/individual")
         .param("count", "0")
+        .session(mockHttpSession)
         .with(csrf()))
-        .andExpect(status().isForbidden());
+        .andExpect(redirectedUrl("/zulassung2/orga/upload-csv"));
   }
 
   @Test
