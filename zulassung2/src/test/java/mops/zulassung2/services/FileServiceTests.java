0c9a959bfe8c9e535ff7cb07698775f2606a4187
loalf100 <loalf100@uni-duesseldorf.de>
2020-03-24 15:45:33 +0100

    readReceiptContentTest

--- zulassung2/src/test/java/mops/zulassung2/services/FileServiceTests.java
+++ zulassung2/src/test/java/mops/zulassung2/services/FileServiceTests.java
@@ -1,20 +1,24 @@
 package mops.zulassung2.services;
 
+import mops.zulassung2.model.crypto.Receipt;
 import mops.zulassung2.model.dataobjects.Student;
 import mops.zulassung2.model.fileparsing.CustomCSVLineParser;
 import mops.zulassung2.model.fileparsing.CustomValidator;
 import mops.zulassung2.model.fileparsing.FileParser;
-import mops.zulassung2.model.minio.CustomNameCreator;
-import mops.zulassung2.model.minio.NameCreator;
+import mops.zulassung2.model.minio.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class FileServiceTests {
   private FileService fileService;
@@ -24,13 +28,21 @@ public class FileServiceTests {
   private NameCreator nameCreator;
   private CustomNameCreator customNameCreator;
   private String receiptContent;
-  private String signature;
   private ReceiptData receiptData;
   private Student louis = new Student("2835749", "loalf100@uni-duesseldorf.de", "alfares", "louis");
   private Student markus = new Student("2757144", "masie@hhu.de", "siewert", "markus");
   private String header = "matriculationnumber,email,name,forename\n";
   private String student1Data = "2727912,tigeu100@hhu.de,geuer,tim\n";
   private String student2Data = "2757144,masie@hhu.de,siewert,markus\n";
+  private  CustomReceiptData customReceiptData;
+  private MinIoImplementation minIo;
+  @Value("${endpoint}")
+  private String endpoint;
+  @Value("${access_key}")
+  private String accessKey;
+  @Value("${secret_key}")
+  private String secretKey;
+
 
   @Test
   public void processCSVUploadTest() throws IOException {
@@ -53,62 +65,72 @@ public class FileServiceTests {
     assertThat(fileService.processCSVUpload(file)).isEqualTo(student);
   }
 
-  @Test
-  public void checkinvalidCSVFormatTest() {
-    // Arrange
-    fileService = new FileService();
-    customValidator = new CustomValidator();
-    customCSVLineParser = new CustomCSVLineParser();
-    List<Student> students = new ArrayList<>();
-    students.add(louis);
-    students.add(markus);
-    String data = header + student1Data + student2Data;
-    byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-
-    FileParser csvParser = new FileParser(customValidator, customCSVLineParser);
-    MockMultipartFile file = new MockMultipartFile("input-file", "test.txt",
-        "text/txt", "test/data".getBytes());
-    // Act
-    List<Student> student = csvParser.processCSV(file);
-    // Assert
-    assertThat(fileService.processCSVUpload(file)).isEqualTo(student);
-  }
 
   @Test
   public void checkprocessTXTUploadTest() {
+    // Arrange
     fileService = new FileService();
     customValidator = new CustomValidator();
+    List<Student> students = new ArrayList<>();
+    students.add(louis);
+    students.add(markus);
+    String data =   header + student1Data + student2Data;
+    byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
     FileParser txtParser = new FileParser(customValidator);
     MockMultipartFile file = new MockMultipartFile("input-file", "test.txt",
-        "input/txt", "test/data".getBytes());
+        "input/txt", bytes);
+    // Act
     List<String> lines = txtParser.processTXT(file);
+    // Assert
     assertThat(fileService.processTXTUpload(file)).isEqualTo(lines);
 
   }
 
+
   @Test
   public void readReceiptContentTest() {
+    //Arrange
     fileService = new FileService();
-    String receiptContent = null;
-    String[] dataObjects = receiptContent.split(" ");
-    Student student = new Student(
-        dataObjects[0].split(":")[1], // Matriculationnumber
-        dataObjects[1].split(":")[1], // Email
-        dataObjects[2].split(":")[1], // Name
-        dataObjects[3].split(":")[1]); // Forename
-    ReceiptData receiptData = new CustomReceiptData(student,
-        dataObjects[4].split(":")[1], // Module
-        dataObjects[5].split(":")[1], // Semester
-        signature);                         // Signature
+    String expectedMatriculationnumber = "9999999";
+    String expectedEmail ="test@uni-duesseldorf.de";
+    String expectedName = "meier";
+    String expectedForename = "max";
+    Student student = new Student(expectedMatriculationnumber, expectedEmail, expectedName,expectedForename);
+    String subject = "informatik";
+    String semester = "zwei";
+    String signature = "abcdefghe";
+    customReceiptData = new CustomReceiptData(student, subject, semester,signature);
 
-    assertThat(fileService.readReceiptContent(receiptContent, signature)).isEqualTo(receiptData);
+    // ACT
+    String receiptContent = customReceiptData.create();
 
+    // Assert
+     ReceiptData ectualData = fileService.readReceiptContent(receiptContent,signature);
+     assertThat(ectualData.getMatriculationNumber()).isEqualTo(expectedMatriculationnumber);
+     assertThat(ectualData.getEmail()).isEqualTo(expectedEmail);
+     assertThat(ectualData.getName()).isEqualTo(expectedName);
+     assertThat(ectualData.getForeName()).isEqualTo(expectedForename);
 
   }
 
   @Test
   public void storeReceiptTest() {
+    File file = mock(File.class);
+    Student student = new Student("9999999", "test@uni-duesseldorf.de",
+        "meier", "max");
+    MinIoRepositoryInterface repo = mock(MinIoRepository.class);
+    MinIoRepository minIo = mock(MinIoRepository.class);
+    NameCreator nameCreator = mock(NameCreator.class);
+    fileService = new FileService();
+    String bucketName = nameCreator.createBucketName(student);
+    when(nameCreator.createBucketName(student)).thenReturn("test-student");
+    when(file.getName()).thenReturn("test-file-name");
+    when(file.getPath()).thenReturn("");
+    when(file.length()).thenReturn(100L);
+    // Act
+    fileService.storeReceipt(student, file);
 
+// Assert
 
   }
 
