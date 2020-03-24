26179e171548c68f05aaebc527deb6768402f240
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-03-24 16:05:29 +0100

    Merge with development

--- zulassung2/src/test/java/mops/zulassung2/services/FileServiceTests.java
+++ zulassung2/src/test/java/mops/zulassung2/services/FileServiceTests.java
@@ -1,11 +1,13 @@
 package mops.zulassung2.services;
 
-import mops.zulassung2.model.crypto.Receipt;
 import mops.zulassung2.model.dataobjects.Student;
-import mops.zulassung2.model.fileparsing.CustomCSVLineParser;
-import mops.zulassung2.model.fileparsing.CustomValidator;
+import mops.zulassung2.model.fileparsing.CSVLineParser;
 import mops.zulassung2.model.fileparsing.FileParser;
-import mops.zulassung2.model.minio.*;
+import mops.zulassung2.model.fileparsing.Validator;
+import mops.zulassung2.model.minio.MinIoImplementation;
+import mops.zulassung2.model.minio.MinIoRepository;
+import mops.zulassung2.model.minio.MinIoRepositoryInterface;
+import mops.zulassung2.model.minio.NameCreator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
@@ -14,19 +16,18 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FileServiceTests {
   private FileService fileService;
-  private CustomValidator customValidator;
-  private CustomCSVLineParser customCSVLineParser;
+  private Validator validator;
+  private CSVLineParser csvLineParser;
   private FileParser csvParser;
   private NameCreator nameCreator;
-  private CustomNameCreator customNameCreator;
   private String receiptContent;
   private ReceiptData receiptData;
   private Student louis = new Student("2835749", "loalf100@uni-duesseldorf.de", "alfares", "louis");
@@ -34,7 +35,6 @@ public class FileServiceTests {
   private String header = "matriculationnumber,email,name,forename\n";
   private String student1Data = "2727912,tigeu100@hhu.de,geuer,tim\n";
   private String student2Data = "2757144,masie@hhu.de,siewert,markus\n";
-  private  CustomReceiptData customReceiptData;
   private MinIoImplementation minIo;
   @Value("${endpoint}")
   private String endpoint;
@@ -48,14 +48,14 @@ public class FileServiceTests {
   public void processCSVUploadTest() throws IOException {
     // Arrange
     fileService = new FileService();
-    customValidator = new CustomValidator();
-    customCSVLineParser = new CustomCSVLineParser();
+    validator = new Validator();
+    csvLineParser = new CSVLineParser();
     List<Student> students = new ArrayList<>();
     students.add(louis);
     students.add(markus);
     String data = header + student1Data + student2Data;
     byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-    FileParser csvParser = new FileParser(customValidator, customCSVLineParser);
+    FileParser csvParser = new FileParser(validator, csvLineParser);
     MockMultipartFile file = new MockMultipartFile("testFile.csv", "test_students.csv",
         "text/csv", bytes);
     // Act
@@ -70,13 +70,13 @@ public class FileServiceTests {
   public void checkprocessTXTUploadTest() {
     // Arrange
     fileService = new FileService();
-    customValidator = new CustomValidator();
+    validator = new Validator();
     List<Student> students = new ArrayList<>();
     students.add(louis);
     students.add(markus);
-    String data =   header + student1Data + student2Data;
+    String data = header + student1Data + student2Data;
     byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-    FileParser txtParser = new FileParser(customValidator);
+    FileParser txtParser = new FileParser(validator);
     MockMultipartFile file = new MockMultipartFile("input-file", "test.txt",
         "input/txt", bytes);
     // Act
@@ -92,24 +92,24 @@ public class FileServiceTests {
     //Arrange
     fileService = new FileService();
     String expectedMatriculationnumber = "9999999";
-    String expectedEmail ="test@uni-duesseldorf.de";
+    String expectedEmail = "test@uni-duesseldorf.de";
     String expectedName = "meier";
     String expectedForename = "max";
-    Student student = new Student(expectedMatriculationnumber, expectedEmail, expectedName,expectedForename);
+    Student student = new Student(expectedMatriculationnumber, expectedEmail, expectedName, expectedForename);
     String subject = "informatik";
     String semester = "zwei";
     String signature = "abcdefghe";
-    customReceiptData = new CustomReceiptData(student, subject, semester,signature);
+    receiptData = new ReceiptData(student, subject, semester, signature);
 
     // ACT
-    String receiptContent = customReceiptData.create();
+    String receiptContent = receiptData.create();
 
     // Assert
-     ReceiptData ectualData = fileService.readReceiptContent(receiptContent,signature);
-     assertThat(ectualData.getMatriculationNumber()).isEqualTo(expectedMatriculationnumber);
-     assertThat(ectualData.getEmail()).isEqualTo(expectedEmail);
-     assertThat(ectualData.getName()).isEqualTo(expectedName);
-     assertThat(ectualData.getForeName()).isEqualTo(expectedForename);
+    ReceiptDataInterface actualData = fileService.readReceiptContent(receiptContent, signature);
+    assertThat(actualData.getMatriculationNumber()).isEqualTo(expectedMatriculationnumber);
+    assertThat(actualData.getEmail()).isEqualTo(expectedEmail);
+    assertThat(actualData.getName()).isEqualTo(expectedName);
+    assertThat(actualData.getForeName()).isEqualTo(expectedForename);
 
   }
 
